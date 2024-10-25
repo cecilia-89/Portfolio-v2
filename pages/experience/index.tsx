e3bcb80f04cbb4c42c028f6ec5818095e91a6bb7
ismaelmiah <ismail27.dec@gmail.com>
2024-10-25 10:00:00 +0600

    Update astha-it internship information

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -293,12 +293,22 @@ const Experience = () => {
             <li className="mb-12">
               <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                 <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                <header
-                  className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
-                  aria-label="July to December 2017"
-                >
-                  December 2020 — January 2021
-                </header>
+                <div className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                  <header aria-label="July to December 2017">
+                    December 2020 — February 2021
+                  </header>
+                  <img
+                    alt=""
+                    loading="lazy"
+                    width="200"
+                    height="48"
+                    decoding="async"
+                    data-nimg="1"
+                    className="rounded transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                    style={{ color: "transparent" }}
+                    src={"/images/experiences/astha-it.jpeg"}
+                  />
+                </div>
                 <div className="z-10 sm:col-span-6">
                   <h3 className="font-medium leading-snug text-slate-200">
                     <div>
